5d22747057c72749cefa8bd120dd4b06ad05264f
Arthur Cohen Costa dos Santo <arthur.santos@guararapes.ind.br>
2019-01-23 15:49:03 -0300

    initial commit

---
+++ Assets/enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isOnGround;
+    public float force = 50f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+        Debug.Log(col.gameObject.tag);
+    }
+
+    public void OnHit(int direction, float hForce, float vForce)
+    {
+        rb.AddForce(new Vector2(force * direction * hForce, force * vForce));
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchControlsKit;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isOnGround;
+    private bool isAtacking;
+    private bool isAtackingCombo;
+    public BoxCollider2D shortAtk;
+    public BoxCollider2D mediumAtk;
+    public BoxCollider2D longAtk;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        shortAtk.enabled = false;
+        mediumAtk.enabled = false;
+        longAtk.enabled = false;
+
+    }
+
+    void FixedUpdate()
+    {
+            if (TCKInput.GetAction("left", EActionEvent.Press)){
+                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, -2.0f, 0.5f), rb.velocity.y);
+            }
+            if (TCKInput.GetAction("right", EActionEvent.Press)){
+                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 2.0f, 0.5f), rb.velocity.y);
+            }
+            if (isOnGround && TCKInput.GetAction("action", EActionEvent.Press)){
+                rb.velocity = new Vector2(rb.velocity.x, 5f);
+            }
+
+            if (TCKInput.GetAction("action2", EActionEvent.Down)){
+                if (!isAtackingCombo && isAtacking){
+                    isAtackingCombo = true;
+                }else if (!isAtacking){
+                    isAtacking = true;
+                    shortAtk.enabled = true;
+                }
+            }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "ground")
+        {
+            isOnGround = true;
+            Debug.Log("chao");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D hit)
+    {
+        if (hit.gameObject.tag == "ground")
+        {
+            isOnGround = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+        Debug.Log("tg");
+        Debug.Log(col.gameObject.tag);
+        if(col.gameObject.tag == "enemy"){
+            if (isAtacking){
+                col.gameObject.GetComponent<enemy>().OnHit(1, 20, 0);
+                isAtacking = false;
+                shortAtk.enabled = false;
+            }
+            if (isAtackingCombo){
+                col.gameObject.GetComponent<enemy>().OnHit(1, 0, 100);
+                isAtackingCombo = false;
+                shortAtk.enabled = false;
+            }
+            Debug.Log("enemy");
+        }
+    }
+}
